dbf3c30aa8fad40124ea51d03b8cbdbb4da7ef65
Linkpool01 <kristopherp@live.com>
2019-09-08 15:53:28 -0400

    Game Audio 1

---
+++ Physics/Assets/SoundManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip Attack, Death, Jump, Kill, Pickup, Run;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Attack = Resources.Load<AudioClip>("Attack");
+        Death = Resources.Load<AudioClip>("Death");
+        Jump = Resources.Load<AudioClip>("Jump");
+        Kill = Resources.Load<AudioClip>("Kill");
+        Pickup = Resources.Load<AudioClip>("Pickup");
+        Run = Resources.Load<AudioClip>("Run");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void Playsound(string clip)
+    {
+        switch (clip)
+        {
+            case "Attack":
+                audioSrc.PlayOneShot(Attack);
+                break;
+            case "Death":
+                audioSrc.PlayOneShot(Death);
+                break;
+            case "Jump":
+                audioSrc.PlayOneShot(Jump);
+                break;
+            case "Kill":
+                audioSrc.PlayOneShot(Kill);
+                break;
+            case "Pickup":
+                audioSrc.PlayOneShot(Pickup);
+                break;
+            case "Run":
+                audioSrc.PlayOneShot(Run);
+                break;
+        }
+    }
+
+}
