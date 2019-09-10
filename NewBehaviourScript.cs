f79dc2be1242136fa75d3c6c4b13f407463c4092
danielL819 <55056322+danielL819@users.noreply.github.com>
2019-09-09 20:32:11 -0400

    Add files via upload
    Updated code

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+   
+    private Rigidbody2D rigid;
+    private int attack;
+    private int damage;
+    
+    void Start()
+    {
+        rigid = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow){
+            print("down key was pressed");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow){
+            print("up key was pressed");
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow) {
+            print("left key was pressed");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) {
+            print("right key was pressed");
+        }
+    }
+}
