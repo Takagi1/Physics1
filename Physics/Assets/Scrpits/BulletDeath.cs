fd8ce24fdf6815542ab89cd02f27e0dc0a958ba1
Linkpool01 <kristopherp@live.com>
2019-09-17 17:23:12 -0400

    Final 2.0

---
+++ Physics/Assets/Scrpits/BulletDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDeath : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Bullet")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
---
+++ Physics/Assets/Scrpits/Death.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.GetComponent<Character>().Health = 0;
+        }
+    }
+}
